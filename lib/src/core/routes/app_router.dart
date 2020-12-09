cae9481c7a0ca2efda5921fbc3a314d73f48f29e
Mar <marlene.gmolina@gmail.com>
2020-12-08 23:56:40 -0600

    ✨ Added details screen route

--- lib/src/core/routes/app_router.dart
+++ lib/src/core/routes/app_router.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/widgets.dart';
 import 'package:rick_and_morty_characters/src/core/routes/named_routes.dart';
 import 'package:rick_and_morty_characters/src/features/catalog/presentation/catalog_screen.dart';
+import 'package:rick_and_morty_characters/src/features/details/data/details_scren_arguments.dart';
+import 'package:rick_and_morty_characters/src/features/details/presentation/details_screen.dart';
 
 class AppRouter {
   Map<String, WidgetBuilder> get routes => {
         NamedRoutes.catalog: (context) => CatalogScreen(),
+        NamedRoutes.details: (context) {
+          final DetailsScreenArguments arguments =
+              ModalRoute.of(context).settings.arguments;
+
+          return DetailsScreen(
+            character: arguments.character,
+          );
+        },
       };
 }
