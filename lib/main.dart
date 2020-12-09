35ce9e271362d420b58330169d600180b5307c58
Mar <marlene.gmolina@gmail.com>
2020-12-08 18:49:04 -0600

    ✨ config app entry point

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty_characters/src/app.dart';
+
+void main() {
+  runApp(App());
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty_characters/src/core/routes/app_router.dart';
+import 'package:rick_and_morty_characters/src/core/routes/named_routes.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final router = AppRouter();
+    return MaterialApp(
+      title: 'Rick & Mory Catalog',
+      routes: router.routes,
+      initialRoute: NamedRoutes.catalog,
+    );
+  }
+}
