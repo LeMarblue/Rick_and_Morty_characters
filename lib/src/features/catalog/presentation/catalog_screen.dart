1643e066eab4f3d829ede825ef4614f973ac7202
Mar <marlene.gmolina@gmail.com>
2020-12-16 17:47:53 -0600

    💄 Added circlar progress indicator

--- lib/src/features/catalog/presentation/catalog_screen.dart
+++ lib/src/features/catalog/presentation/catalog_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:rick_and_morty_characters/src/core/utils/api_client.dart';
 import 'package:rick_and_morty_characters/src/core/data/character_model.dart';
 import 'package:rick_and_morty_characters/src/core/routes/named_routes.dart';
-import 'package:rick_and_morty_characters/src/features/catalog/blocs/catalog_boc.dart';
+import 'package:rick_and_morty_characters/src/features/catalog/blocs/catalog_bloc.dart';
 import 'package:rick_and_morty_characters/src/features/details/data/details_scren_arguments.dart';
 
 class CatalogScreen extends StatelessWidget {
@@ -35,6 +34,18 @@ class CatalogScreen extends StatelessWidget {
                 itemCount: characters.length,
                 itemBuilder: (context, index) {
                   final character = characters[index];
+                  if (index == characters.length - 1) {
+                    catalogBloc.getCharacters();
+                    if (catalogBloc.isfinalPage == false) {
+                      return Container(
+                        padding: EdgeInsets.only(bottom: 12),
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    }
+                  }
                   return ListTile(
                     onTap: () {
                       Navigator.of(context).pushNamed(
@@ -46,7 +57,9 @@ class CatalogScreen extends StatelessWidget {
                     },
                     leading: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Image.network(character.image),
+                      child: Image.network(
+                        character.image,
+                      ),
                     ),
                     title: Text('${character.name}'),
                   );
