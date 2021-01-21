67648da1915360aa031233dc1d02c030a6ddca3a
Mar <marlene.gmolina@gmail.com>
2021-01-21 14:03:18 -0600

    💄 Force fixed  Portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rick_and_morty_characters/src/app.dart';
 
 void main() {
-  runApp(App());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) => runApp(App()));
 }
